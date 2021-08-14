a6f4dc2fc6a3299f6071186aa0dcded3086ec2d4
devsunset <devsunset@gmail.com>
2021-08-14 21:56:59 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # tgm
-tgm backend
---
+++ db/db.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"tgm/config"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+var err error
+
+func DbManager() *sql.DB {
+	configuration := config.GetConfig()
+
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", configuration.DB_HOST, configuration.DB_PORT, configuration.DB_USERNAME, configuration.DB_PASSWORD, configuration.DB_NAME)
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.SetMaxIdleConns(configuration.DB_MAXIDLECONNS)
+	db.SetMaxOpenConns(configuration.DB_MAXOPENCONNS)
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	return db
+}
