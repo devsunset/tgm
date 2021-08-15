d8ee527ce605c54aba731c54db3582ae05766ee3
devsunset <devsunset@gmail.com>
2021-08-15 20:37:31 +0900

    dev

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.16
 
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
-	github.com/go-openapi/spec v0.20.3 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.2.0
 	github.com/lib/pq v1.9.0
@@ -12,10 +11,9 @@ require (
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/sirupsen/logrus v1.8.0
 	github.com/swaggo/echo-swagger v1.1.0
-	github.com/swaggo/swag v1.7.0
+	github.com/swaggo/swag v1.7.1
 	github.com/tkanos/gonfig v0.0.0-20210106201359-53e13348de2f
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210303074136-134d130e1a04 // indirect
-	golang.org/x/tools v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,20 +3,24 @@
 	go install & path setting -> https://golang.org/
 	EchoHigh performance, extensible, minimalist Go web framework
 	https://echo.labstack.com/
-	mkdir -p /workspace/devwork/tgm
-	cd /workspace/devwork/tgm
+	mkdir -p <work_path>/tgm
+	cd <work_path>/tgm
+
 	go mod init tgm
 	go get github.com/labstack/echo/v4
 	go get github.com/labstack/echo/middleware
 	go mod download golang.org/x/time
+
 	coding main.go (swagger_apply_pre_main.go)
 	https://pkg.go.dev/github.com/amarnus/echo-swagger#readme-echo-swagger
+
 	go get github.com/swaggo/swag/cmd/swag
-	cd /workspace/devwork/tgm
+	cd <work_path>/tgm
 	$GOPATH/go/bin/swag init (in project directory)
 	go get -u github.com/swaggo/echo-swagger
 	go mod download github.com/alecthomas/template
 	go get github.com/alecthomas/template@v0.0.0-20190718012654-fb15b899a751
+
 	coding main.go
         https://github.com/swaggo/swag#declarative-comments-format
 */
@@ -46,7 +50,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host 132.145.85.185
+// @host 127.0.0.1
 // @BasePath /
 func main() {
 
