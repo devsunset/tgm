e987146ca8265ce7c745c6f349b4e3a530f72fd0
devsunset <devsunset@gmail.com>
2021-08-30 15:32:08 +0900

    dev

--- prototype/linux-cli.go
+++ prototype/linux-cli.go
@@ -3,21 +3,36 @@ package main
 import (
 	"bufio"
 	"crypto/rand"
-	"encoding/base64"
+	// "encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+	"os/user"
 	"os/exec"
 	"strings"
+)
+
+////////////////////////////////////////
+const (
+	groupFile string = "/etc/group"
 )
 
 const (
 	userFile string = "/etc/passwd"
 )
 
+type Groups struct {
+	Groups []Group `json:"groups"`
+}
+
 type Users struct {
 	Users []User `json:"users"`
+}
+
+type Group struct {
+	Group     string `json:group`
 }
 
 type User struct {
@@ -26,9 +41,10 @@ type User struct {
 	Group     string `json:group`
 	Shell     string `json:shell`
 }
+////////////////////////////////////////
 
 // Read json file and return slice of byte.
-func ReadUsers(f string) []byte {
+func ReadJsonFile(f string) []byte {
 	jsonFile, err := os.Open(f)
 
 	if err != nil {
@@ -38,6 +54,24 @@ func ReadUsers(f string) []byte {
 
 	data, _ := ioutil.ReadAll(jsonFile)
 	return data
+}
+
+// Read file /etc/group and return slice of groups
+func ReadEtcGroup(f string) (list []string) {
+	file, err := os.Open(f)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	r := bufio.NewScanner(file)
+
+	for r.Scan() {
+		lines := r.Text()
+		parts := strings.Split(lines, ":")
+		list = append(list, parts[0])
+	}
+	return list
 }
 
 // Read file /etc/passwd and return slice of users
@@ -59,7 +93,7 @@ func ReadEtcPasswd(f string) (list []string) {
 }
 
 // Check if user on the host
-func check(s []string, u string) bool {
+func checkAccount(s []string, u string) bool {
 	for _, w := range s {
 		if u == w {
 			return true
@@ -79,9 +113,46 @@ func CreateRandom(n int) string {
 	return string(b)
 }
 
+////////////////////////////////////////
+
+// Group is created by executing shell command groupadd
+func AddGroup(g *Group) (bool) {
+	argGroup := []string{g.Group}
+	
+	groupCmd := exec.Command("groupadd", argGroup...)
+
+	fmt.Println(groupCmd)
+
+	if out, err := groupCmd.Output(); err != nil {		
+		fmt.Println(err, "There was an error by adding group", g.Group)
+		return false
+	} else {
+		fmt.Printf("Output: %s\n", out)
+		return true
+	}
+}
+
+// Group is deleted by executing shell command groupdel
+func DeleteGroup(g *Group) (bool) {
+	argGroup := []string{g.Group}
+	
+	groupCmd := exec.Command("groupdel", argGroup...)
+
+	fmt.Println(groupCmd)
+
+	if out, err := groupCmd.Output(); err != nil {		
+		fmt.Println(err, "There was an error by deleting group", g.Group)
+		return false
+	} else {
+		fmt.Printf("Output: %s\n", out)
+		return true
+	}
+}
+
 // User is created by executing shell command useradd
-func AddNewUser(u *User) (bool, string) {
-	encrypt := base64.StdEncoding.EncodeToString([]byte(CreateRandom(9)))
+func AddNewUser(u *User) (bool, string) {	
+	// encrypt := base64.StdEncoding.EncodeToString([]byte(CreateRandom(9)))
+	encrypt := "test1qazxsw2"
 
 	argUser := []string{"-m", "-d", u.Directory, "-G", u.Group, "-s", u.Shell, u.Name}
 	argPass := []string{"-c", fmt.Sprintf("echo %s:%s | chpasswd", u.Name, encrypt)}
@@ -89,13 +160,16 @@ func AddNewUser(u *User) (bool, string) {
 	userCmd := exec.Command("useradd", argUser...)
 	passCmd := exec.Command("/bin/sh", argPass...)
 
-	if out, err := userCmd.Output(); err != nil {
+	fmt.Println(userCmd)
+	fmt.Println(passCmd)
+
+	if out, err := userCmd.Output(); err != nil {		
 		fmt.Println(err, "There was an error by adding user", u.Name)
 		return false, ""
 	} else {
 		fmt.Printf("Output: %s\n", out)
-
 		if _, err := passCmd.Output(); err != nil {
+			
 			fmt.Println(err)
 			return false, ""
 		}
@@ -103,19 +177,134 @@ func AddNewUser(u *User) (bool, string) {
 	}
 }
 
+// User is deleted by executing shell command userdel
+func DeleteUser(u *User) (bool) {	
+
+	argUser := []string{u.Name}
+	// argUser := []string{"-r", u.Name}
+
+
+	userCmd := exec.Command("userdel", argUser...)
+
+	fmt.Println(userCmd)
+
+	if out, err := userCmd.Output(); err != nil {		
+		fmt.Println(err, "There was an error by deleting user", u.Name)
+		return false
+	} else {
+		fmt.Printf("Output: %s\n", out)
+		return true
+	}
+}
+
+////////////////////////////////////////
+
 func groupadd() {
-	fmt.Println("groupadd  groupadd.json execute ....")
+	fmt.Println("groupadd jsonfile/group.json execute ....")
+
+	NameOfFile := "jsonfile/group.json"
+	data := ReadJsonFile(NameOfFile)
+
+	var g Groups
+	json.Unmarshal(data, &g)
+
+	groupList := ReadEtcGroup(groupFile)
+
+	for i := range g.Groups {
+		c := checkAccount(groupList, g.Groups[i].Group)
+		if c == false {
+			if info := AddGroup(&g.Groups[i]); info == true {
+				fmt.Println("Group was added:>", g.Groups[i].Group)
+			}
+		} else {
+			fmt.Println("The group already exists:>", g.Groups[i].Group)
+		}
+	}
 }
 
 func groupdel() {
-	fmt.Println("groupdel groupdel.json execute ....")
+	fmt.Println("groupdel jsonfile/group.json execute ....")
+
+	NameOfFile := "jsonfile/group.json"
+	data := ReadJsonFile(NameOfFile)
+
+	var g Groups
+	json.Unmarshal(data, &g)
+
+	groupList := ReadEtcGroup(groupFile)
+
+	for i := range g.Groups {
+		c := checkAccount(groupList, g.Groups[i].Group)
+		if c == false {
+			fmt.Println("The group is not exists:>", g.Groups[i].Group)
+		} else {
+			
+			if info := DeleteGroup(&g.Groups[i]); info == true {
+				fmt.Println("Group was deleted:>", g.Groups[i].Group)
+			}
+		}
+	}
 }
 
-func useradd() {
-	fmt.Println("useradd useradd.json execute ....")
+func getgroups() {
+	fmt.Println("getgroups execute ....")
 
-	NameOfFile := "useradd.json"//os.Args[1]
-	data := ReadUsers(NameOfFile)
+	var LinuxGroups []string
+
+	// this is for Linux/Unix machines
+	file, err := os.Open(groupFile)
+
+	if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+	}
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	for {
+			line, err := reader.ReadString('\n')
+
+			if equal := strings.Index(line, "#"); equal < 0 {
+					lineSlice := strings.FieldsFunc(line, func(divide rune) bool {
+							return divide == ':' 
+					})
+
+					if len(lineSlice) > 0 {
+						LinuxGroups = append(LinuxGroups, lineSlice[0])
+					}
+			}
+
+			if err == io.EOF {
+					break
+			}
+			if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+			}
+
+	}
+
+	for _, name := range LinuxGroups {			
+			usr, err := user.LookupGroup(name)
+			if err != nil {
+					panic(err)
+			}
+			// see https://pkg.go.dev/os/user#Group
+			fmt.Printf("NAME:%s\n", usr.Name)
+			fmt.Printf("GID:%s\n", usr.Gid)
+			fmt.Println("*********************************")
+	}
+}
+
+////////////////////////////////////////
+
+func useradd() {
+	fmt.Println("useradd user.json execute ....")
+
+	NameOfFile := "jsonfile/user.json"
+	data := ReadJsonFile(NameOfFile)
 
 	var u Users
 	json.Unmarshal(data, &u)
@@ -123,7 +312,7 @@ func useradd() {
 	userList := ReadEtcPasswd(userFile)
 
 	for i := range u.Users {
-		c := check(userList, u.Users[i].Name)
+		c := checkAccount(userList, u.Users[i].Name)
 		if c == false {
 			if info, passwd := AddNewUser(&u.Users[i]); info == true {
 				fmt.Println("User was added:>", u.Users[i].Name, "=>", "Password:>", passwd)
@@ -135,18 +324,93 @@ func useradd() {
 }
 
 func userdel() {
-	fmt.Println("userdel userdel.json execute ....")
+	fmt.Println("userdel user.json execute ....")
+
+	NameOfFile := "jsonfile/user.json"
+	data := ReadJsonFile(NameOfFile)
+
+	var u Users
+	json.Unmarshal(data, &u)
+
+	userList := ReadEtcPasswd(userFile)
+
+	for i := range u.Users {
+		c := checkAccount(userList, u.Users[i].Name)
+		if c == false {
+			fmt.Println("The user not exists:>", u.Users[i].Name)
+		} else {
+			
+			if info := DeleteUser(&u.Users[i]); info == true {
+				fmt.Println("User was deleted:>", u.Users[i].Name)
+			}
+		}
+	}
 }
 
-func main() {
-	encrypt := base64.StdEncoding.EncodeToString([]byte(CreateRandom(3)))
-	fmt.Println(encrypt)
+func getusers() {
+	fmt.Println("getusers execute ....")
 
+	var LinuxUsers []string
+
+	// this is for Linux/Unix machines
+	file, err := os.Open(userFile)
+
+	if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+	}
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	for {
+			line, err := reader.ReadString('\n')
+
+			if equal := strings.Index(line, "#"); equal < 0 {
+					lineSlice := strings.FieldsFunc(line, func(divide rune) bool {
+							return divide == ':' 
+					})
+
+					if len(lineSlice) > 0 {
+							LinuxUsers = append(LinuxUsers, lineSlice[0])
+					}
+
+			}
+
+			if err == io.EOF {
+					break
+			}
+			if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+			}
+
+	}
+
+	for _, name := range LinuxUsers {			
+			usr, err := user.Lookup(name)
+			if err != nil {
+					panic(err)
+			}
+			// see https://golang.org/pkg/os/user/#User			
+			fmt.Printf("username:%s\n", usr.Username)
+			fmt.Printf("homedir:%s\n", usr.HomeDir)
+			fmt.Printf("UID:%s\n", usr.Uid)
+			fmt.Printf("GID:%s\n", usr.Gid)
+			fmt.Printf("DisplayName:%s\n", usr.Name)
+			fmt.Println("*********************************")
+	}
+}
+
+////////////////////////////////////////
+
+func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("====================================")
 		fmt.Println("### Usage ###")
 		fmt.Println("")
-		fmt.Println("go run linux-cli.go <<command>>")
+		fmt.Println("    go run linux-cli.go <<command>>")
 		fmt.Println("ex) go run linux-cli.go useradd")
 		fmt.Println("")
 
@@ -155,8 +419,11 @@ func main() {
 		fmt.Println("====================================")
 		fmt.Println("groupadd")
 		fmt.Println("groupdel")
+		fmt.Println("getgroups")
+		fmt.Println("====================================")
 		fmt.Println("useradd")
 		fmt.Println("userdel")
+		fmt.Println("getusers")
 		fmt.Println("====================================")
 		os.Exit(1)
 	}
@@ -165,10 +432,14 @@ func main() {
 		groupadd()
 	} else if os.Args[1] == "groupdel" {
 		groupdel()
+	} else if os.Args[1] == "getgroups" {
+		getgroups()
 	} else if os.Args[1] == "useradd" {
 		useradd()
 	} else if os.Args[1] == "userdel" {
 		userdel()
+	} else if os.Args[1] == "getusers" {
+		getusers()
 	} else {
 		fmt.Println("Invalid command args")
 		os.Exit(1)
