092a95dc89a3e2a51df2bdd4d4ed2a37413408e9
devsunset <devsunset@gmail.com>
2021-09-06 16:49:10 +0900

    dev

--- frontend/src/components/settings/Permissions.vue
+++ frontend/src/components/settings/Permissions.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>{{ $t("settings.permissions") }}</h3>
-    <p class="small">{{ $t("settings.permissionsHelp") }}</p>
+    <p v-show="false" class="small">{{ $t("settings.permissionsHelp") }}</p>
 
     <p>
       <input type="checkbox" v-model="admin" disabled/>
