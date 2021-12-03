a140d35f8309bc381d1203e5ccc71a6b14b4cf8d
devsunset <devsunset@gmail.com>
2021-12-03 19:22:10 +0900

    dev

--- tgm_build/stop_tgm.sh
+++ tgm_build/stop_tgm.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+sudo kill -9 $(ps aux | grep 'webssh2' | awk '{print $2}')
+echo "tgm service stop ..."
+
 sudo kill -9 $(ps aux | grep 'tgm' | awk '{print $2}')
 echo "tgm service stop ..."
 
-sudo kill -9 $(ps aux | grep 'webssh2' | awk '{print $2}')
-echo "tgm service stop ..."
 
 
